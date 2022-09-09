dd0a650d99e1210dd48cd68cf87fd92d8773ee42
Oluwaseyi Aaron <oluwaseyiaaron@gmail.com>
2022-09-08 23:22:20 -0700

    puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
-
 /**
- * main - Entry Point
- *
- * Description: prints "programming is like building a mulitilingual puzzle"
- *
- * Return: always 0 (success)
-*/
+ * main - main block
+ * Return: 0
+ */
 int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
