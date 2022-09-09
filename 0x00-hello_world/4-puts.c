d2ab0cec1623867e3bf922694d5b9a027c1c0ecb
Oluwaseyi Aaron <oluwaseyiaaron@gmail.com>
2022-09-08 23:09:46 -0700

    put.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry Point
  *
- * Description: prints 'programming is like a building a mulitilingual puzzle
+ * Description: prints "programming is like building a mulitilingual puzzle"
  *
  * Return: always 0 (success)
 */
