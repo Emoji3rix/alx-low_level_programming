8df62b5aeaf636e9a03df425156ab721d0b2c33d
Oluwaseyi Aaron <oluwaseyiaaron@gmail.com>
2022-09-09 20:04:39 -0700

    print alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - print the letters of the alphabet
+ * Return: 0
+ */
+
+int main(void)
+{
+	int b;
+	int j;
+	
+	b = 97;
+	j = 65;
+
+	while (b < 123)
+	{
+		putchar(b);
+		b++;
+	}
+
+	while (j < 91)
+	{
+		putchar(j);
+		j++;
+	}
+
+	putchar(10);
+
+	return (0);
+}
