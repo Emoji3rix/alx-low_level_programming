98b3d0568b666f74a3fb2235caa41e833dad2b49
Oluwaseyi Aaron <oluwaseyiaaron@gmail.com>
2022-09-13 20:08:20 -0700

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isalpha - checks if its an alphabet
+ * 
+ * Return: return 1 if is an alphabet or 0 if otherwise
+ */
+int _isalpha(int c)
+{
+
+	if( (c >= 'a' && c <= 'z') ||
+	    (c >= 'A' && c <= 'Z') )
+		return (1);
+	else
+		return (0);
+}
