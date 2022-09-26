6d7e67fab2b4868b4b4490f907ebaa624669eeb8
Oluwaseyi Aaron <oluwaseyiaaron@gmail.com>
2022-09-26 15:42:56 -0700

    1-memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -11,10 +11,13 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	int i;
+	unsigned int i;
 
-	for (int i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
+	{
 		dest[i] = src[i];
+	}
+
 
 	return (dest);
 }
