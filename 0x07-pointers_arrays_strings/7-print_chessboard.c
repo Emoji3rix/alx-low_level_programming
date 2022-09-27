4150814adf439f5d946e623c68ead956ac094c8d
Oluwaseyi Aaron <oluwaseyiaaron@gmail.com>
2022-09-27 15:26:34 -0700

    7-print_chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -5,12 +5,11 @@
  * @a: chessboard
  * Return: void
  */
-
 void print_chessboard(char (*a)[8])
 {
 	int i, j;
 
-	for (i = 0; 1 < 8; i++)
+	for (i = 0; i < 8; i++)
 	{
 		for (j = 0; j < 8; j++)
 		{
