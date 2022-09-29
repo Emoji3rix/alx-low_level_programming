1934706a3114be2e96545827f6996bc75202f733
Oluwaseyi Aaron <oluwaseyiaaron@gmail.com>
2022-09-29 16:19:40 -0700

    0-puts_recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts-recursion - prints a string the then a new line
+ * @s: pointer to the string
+ * Retun: void
+ */
+void _put_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+	else 
+	{
+		_putchar(*s);
+		_put_recursion(s + 1);
+	}
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,5 @@
+#Recursion
+
+what is recursion
+how to implement recursion
+in what situation you should implement recursion 
