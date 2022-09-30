f2c0b47ed75cac9e98e008551491d6f6d0d93e1c
Oluwaseyi Aaron <oluwaseyiaaron@gmail.com>
2022-09-29 18:11:12 -0700

    100-is_palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,92 +1,57 @@
 #include "main.h"
-#include <stdio.h>
-
 
 /**
- * wildcmp - check the code for Holberton School students.
+ * palind2 - obtains length of a
  *
- * @s1: string
+ * @a: string
  *
- * @s2: string
+ * @l: integer to count length
  *
- * Return: Always 0.
- *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately
  */
 
-
-
-int wildcmp(char *s1, char *s2)
+int palind2(char *a, int l)
 {
-	return (checker(s1, s2, 0, 0, -1));
+	if (*a == 0)
+		return (l - 1);
+	return (palind2(a + 1, l + 1));
 
 }
 
+
 /**
- * checkLast - check last char of s2 when s1 ends
+ * palind3 - compares string vs string reverse
  *
- * @s: string
+ * @a: string
  *
- * @i: int
+ * @l: length
  *
- * Return: 0 or 1
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-
-int checkLast(char *s, int i)
+int palind3(char *a, int l)
 {
-	if (s[i] == '*')
-		return (checkLast(s, i + 1));
-
-	else if (s[i] == '\0')
+	if (*a != *(a + l))
+		return (0);
+	else if (*a == 0)
 		return (1);
-
-	return (0);
-
+	return (palind3(a + 1, l - 2));
 }
 
 /**
- * checker - helper
+ * is_palindrome - checks if a string is a palindrome
+ * @s: string to evaluate
  *
- * @s1: string
+ * Return: On success 1.
  *
- * @s2: string
- *
- * @a: int
- *
- * @b: int
- *
- * @wildUsed: int
- *
- * Return: Always 0.
- *
+ * On error, -1 is returned, and errno is set appropriately.
  */
 
-int checker(char *s1, char *s2, int a, int b, int wildUsed)
+int is_palindrome(char *s)
 {
-	if (s1[a] != '\0')
-	{
-		if (s2[b] == '\0')
-			return (0);
-		else if (s2[b] == '*')
-		{
-			if (s2[b + 1] == '*')
-				return (checker(s1, s2, a, b + 1, b));
-			else if (s2[b + 1] == s1[a])
-				return (checker(s1, s2, a, b + 1, b));
-			else if (s1[a + 1] != s2[b + 1])
-				return (checker(s1, s2, a + 1, b, b));
-			else if (s1[a + 1] == s2[b + 1])
-				return (checker(s1, s2, a + 1, b + 1, b));
-			else if ((s1[a] == s2[b]) || (s2[b] == '*' && s2[b + 1] == s1[a + 1]))
-				return (checker(s1, s2, a + 1, b + 1, wildUsed));
+	int l;
 
-			if (wildUsed == -1)
-				return (0);
-			return (checker(s1, s2, a, wildUsed, wildUsed));
-		}
-
-	if (s2[b] != '\0')
-		return (checkLast(s2, b));
-
-	return (1);
-	}
+	l = palind2(s, 0);
+	return (palind3(s, l));
 }
